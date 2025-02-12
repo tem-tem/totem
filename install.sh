29b9b615cae90dcc6471c2ca644d4469e128f2e0
Tem <t3mir1an@gmail.com>
2025-02-12 12:32:56 -0600

    fix quote

--- install.sh
+++ install.sh
@@ -4,4 +4,4 @@ curl -L -o totem https://raw.githubusercontent.com/tem-tem/totem/main/totem
 chmod +x totem
 sudo mv totem /usr/local/bin/
 echo "Totem installed! Run 'totem t 12:30' to test."
-echo "Run 'totem c \"2 + 3 * (2 + 22)/2\" to test."
+echo "Run 'totem c \"2 + 3 * (2 + 22)/2\"' to test."
